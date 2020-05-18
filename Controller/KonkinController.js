1da5b112794c2e562d7d354f9784bdd7a0879cd4
Bayu Novaldi <36811359+idlavoNuyaB@users.noreply.github.com>
2020-05-18 21:58:23 +0700

    fix minor problem in konkin

--- Controller/KonkinController.js
+++ Controller/KonkinController.js
@@ -3,17 +3,12 @@ var router = express.Router();
 var sql = require("mssql");
 var config = require("../Connection/config");
 
-var executeQuery = function(res,query,cek,parameters){
+var executeQuery = function(res,query){
   // connect to your database
   sql.connect(config, function (err) {
     if (err) console.log(err);
     // create Request object
     var request = new sql.Request();
-    if(cek == 0){
-      parameters.forEach(function(p){
-        request.input(p.name,p.sqltype,p.value);
-      });
-    }
     // query to the database and get the records
     request.query(query, function (err, recordset) {
         if (err) console.log(err)
@@ -26,13 +21,8 @@ var executeQuery = function(res,query,cek,parameters){
 
 var routes = function(){
   router.route('/').get(function(req,res){
-    var query = 'Select Aspek.aspek as Aspek, Aspek.komponen_aspek as [Komponen Aspek],' +
-    'MasterIndikator.nama as [Indikator Kinerja],Indikator_SatuanKerja.bobot as Bobot,' +
-    'Indikator_SatuanKerja.target as Target,Indikator_SatuanKerja.capaian as Capaian,' +
-    'SatuanKerja.nama as Satuan from ((MasterIndikator INNER JOIN Indikator_SatuanKerja ON' +
-    'Indikator_Satuankerja.id_master = MasterIndikator.id) INNER JOIN Aspek ON Aspek.id = '+
-    'MasterIndikator.id_aspek) INNER JOIN SatuanKerja ON Indikator_SatuanKerja.id_satker = SatuanKerja.id';
-    executeQuery(res,query,0,0);
+    var query = 'Select * from konkin';
+    executeQuery(res,query);
   });
   return router
 };
